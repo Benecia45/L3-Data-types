a66052c23ced4a1ad510b3484192ea337a1f68ff
Benecia45 <frempong2007@gmail.com>
2024-09-29 18:50:21 +0000

    data tpes and type casting

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Benecia"
+grade = 12 
+height = -2.7
+goes_to_church = True 
+
+print(type(name))
+print(type(goes_to_church))
+
+height = bool(height)
+print(type(height))
+print(height)
+
+grade = str(grade)
+print(type(grade))
+print(grade)
